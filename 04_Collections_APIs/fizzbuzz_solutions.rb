53773bcb2f4e50d1494fe5e8a3e2e08ce7215e76
Colt Steele <coltsteele1@gmail.com>
2014-09-17 18:42:19 -0700

    fix fizzbuzz sol

--- 04_Collections_APIs/fizzbuzz_solutions.rb
+++ 04_Collections_APIs/fizzbuzz_solutions.rb
@@ -1,15 +1,27 @@
 #One Possible Solution:
-1.upto(100) do |i|
-  if i % 5 == 0 && i % 3 == 0
-    puts "FizzBuzz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  else
-    puts i
-  end
-end
+# 1.upto(100) do |i|
+#   if i % 5 == 0 && i % 3 == 0
+#     puts "FizzBuzz"
+#   elsif i % 5 == 0
+#     puts "Buzz"
+#   elsif i % 3 == 0
+#     puts "Fizz"
+#   else
+#     puts i
+#   end
+# end
+
+# 1.upto(100) do |i|
+#   if i % 5 == 0 && i% 3 != 0
+#     puts "Buzz"
+#   elsif i % 5 == 0 && i % 3 == 0
+#     puts "FizzBuzz"
+#   elsif i % 3 == 0
+#     puts "Fizz"
+#   else
+#     puts i
+#   end
+# end
 
 #Another Solution:
 # def fizzbuzz(maximum)
@@ -21,3 +33,22 @@ end
 #     puts line
 #   end
 # end
+
+# fizzbuzz(754)
+
+# puts "*****" 
+# puts "\n" * 10
+
+# fizzbuzz(100)
+
+1.upto(100).each do |n|
+  line = ''
+  # line << 'Fizz' if n % 3 == 0
+  if n % 3 == 0
+    line << 'Fizz' 
+  end
+  line << 'Buzz' if n % 5 == 0
+  line = n if line.empty?
+  puts line
+end
+
