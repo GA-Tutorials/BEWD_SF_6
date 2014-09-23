2b19a75883f2bbc057d98ca9fa5cbe1354d67155
Kisha Richardson <kisha@spunklabs.com>
2014-09-22 17:48:27 -0700

    upload class_5

---
+++ 05_Classes_Objects/starter_code/ex_secret_number_objects/lib/game.rb
@@ -0,0 +1,9 @@
+require_relative 'lib/person'
+require_relative 'lib/secret_number'
+
+class Game
+# this class will be complex
+# we need to write logic to initialize a new game, and run the game loop
+# we'll want to write a few separate methods for unique tasks so that our
+# code is structured properly
+end
